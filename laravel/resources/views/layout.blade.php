6f679974b4e6d58809129a206ff2a0741572b72f
Drifter <jacobsenpalouda@gmail.com>
2024-11-05 15:15:11 +0100

    Created and moved code to layout.css

--- laravel/resources/views/layout.blade.php
+++ laravel/resources/views/layout.blade.php
@@ -4,10 +4,9 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>@yield('title') - PETAPP</title>
-    <link rel="stylesheet" href="{{ asset('assets/css/layout.css') }}">
     <link rel="stylesheet" href="{{ asset('assets/css/app.css') }}">
     <link rel="icon" href="{{ asset('assets/photos/PetApp-favicon.png') }}" type="image/x-icon">
-    <link rel="stylesheet" href="../../assets/css/tempBucket.css">
+    <link rel="stylesheet" href="{{ asset('assets/css/layout.css') }}">
 </head>
 <body>
     <!-- Navbar Section -->
