fab5d67800eac94afa4550941efad80c739c2128
Drifter <jacobsenpalouda@gmail.com>
2024-11-07 15:12:30 +0100

    added registration route to web.php

--- laravel/routes/web.php
+++ laravel/routes/web.php
@@ -6,15 +6,25 @@ use App\Http\Controllers\AuthController;
 Route::get('/', function () {
     return view('welcome');
 });
+
 Route::get('/about', function () {
     return view('about');
-});Route::get('/contacts', function () {
+});
+Route::get('/contacts', function () {
     return view('contacts');
-});Route::get('/gallery', function () {
+});
+
+Route::get('/gallery', function () {
     return view('gallery');
 });
+
 Route::get('/more', function () {
     return view('more');
 });
+
+Route::get('/register', function () {
+    return view('registration');
+});
+
 Route::post('/login', [AuthController::class, 'login'])->name('login');
    
