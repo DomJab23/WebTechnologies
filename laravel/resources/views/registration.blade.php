4ee8d1ab1c6c250ad1e69f98c23a054c949dee12
Drifter <jacobsenpalouda@gmail.com>
2024-11-07 15:05:07 +0100

    Added inputs

--- laravel/resources/views/registration.blade.php
+++ laravel/resources/views/registration.blade.php
@@ -4,4 +4,12 @@
         <title>Register</title>
         <link rel="stylesheet" href="../../assets/css/registration.ss">
     </head>
+
+    <body>
+        <form action="">
+            <input type="text" name="uname" placeholder="Username">
+            <input type="text" name="password" placeholder="Password">
+            <input type="text" name="code" placeholder="Code">
+        </form>
+    </body>
 </html>
