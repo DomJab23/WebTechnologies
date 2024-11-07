909a091b8895a26b04fde5fa89106308f1daa0f1
Drifter <jacobsenpalouda@gmail.com>
2024-11-07 16:44:37 +0100

    Registration page styling

--- laravel/resources/views/registration.blade.php
+++ laravel/resources/views/registration.blade.php
@@ -1,17 +1,28 @@
 <!DOCTYPE html>
 <html lang="en">
     <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <title>Register</title>
+        <link rel="stylesheet" href="../../assets/css/textColors.css">
         <link rel="stylesheet" href="../../assets/css/registration.css">
     </head>
 
     <body>
-        <form action="{{ route('registerPush') }}" method="post">
-            @csrf
-            <input type="text" name="uname" placeholder="Username">
-            <input type="password" name="password" placeholder="Password">
-            <input type="text" name="code" placeholder="Code">
-            <button>Submit</button>
-        </form>
+        <div class="centerBox">
+            <form action="{{ route('registerPush') }}" method="post">
+                @csrf
+                <p class="chooseText darkOrange">Choose a username:</p>
+                <input type="text" name="uname" placeholder="Username">
+
+                <p class="chooseText darkOrange">Choose a password:</p>
+                <input type="password" name="password" placeholder="Password">
+
+                <p class="chooseText darkOrange">Enter your code:</p>
+                <input type="text" name="code" placeholder="Code">
+                
+                <button>Submit</button>
+            </form>
+        </div>
     </body>
 </html>
