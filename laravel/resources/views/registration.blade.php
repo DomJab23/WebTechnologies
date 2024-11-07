be4a47c959e4efe8deb2ee0e503aa39702a606bd
Drifter <jacobsenpalouda@gmail.com>
2024-11-07 15:15:29 +0100

    Added registration php and css files

---
+++ laravel/resources/views/registration.blade.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <title>Register</title>
+        <link rel="stylesheet" href="../../assets/css/registration.css">
+    </head>
+
+    <body>
+        <input type="text" name="uname" placeholder="Username">
+        <input type="text" name="password" placeholder="Password">
+        <input type="text" name="code" placeholder="Code">
+    </body>
+</html>
