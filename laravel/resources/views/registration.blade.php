d076e80fc5c044647223c6f8e8b4954b43d39657
Erschh <tomecsko.ors@freemail.hu>
2024-11-08 11:01:12 +0100

    Registration Styling

--- laravel/resources/views/registration.blade.php
+++ laravel/resources/views/registration.blade.php
@@ -8,21 +8,23 @@
         <link rel="stylesheet" href="../../assets/css/registration.css">
     </head>
 
-    <body>
+    <body class="backImage">
         <div class="centerBox">
-            <form action="{{ route('registerPush') }}" method="post">
-                @csrf
-                <p class="chooseText darkOrange">Choose a username:</p>
-                <input type="text" name="uname" placeholder="Username">
+            <div class="backDrop">
+                <form action="{{ route('registerPush') }}" method="post">
+                    @csrf
+                    <p class="chooseText darkOrange">Choose a username:</p>
+                    <input type="text" name="uname" placeholder="Username">
 
-                <p class="chooseText darkOrange">Choose a password:</p>
-                <input type="password" name="password" placeholder="Password">
+                    <p class="chooseText darkOrange">Choose a password:</p>
+                    <input type="password" name="password" placeholder="Password">
 
-                <p class="chooseText darkOrange">Enter your code:</p>
-                <input type="text" name="code" placeholder="Code">
-                
-                <button>Submit</button>
-            </form>
+                    <p class="chooseText darkOrange">Enter your code:</p>
+                    <input type="text" name="code" placeholder="Code">
+                    <br>
+                    <button>Register</button>
+                </form>
+            </div>
         </div>
     </body>
 </html>
