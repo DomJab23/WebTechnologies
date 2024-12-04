12de671e2a0179590a942ef007d7cc30e9a90ec3
RadekChy25 <chylak.radoslaw@gmail.com>
2024-12-04 13:50:24 +0100

    Volunteer page connection

--- laravel/resources/views/layout.blade.php
+++ laravel/resources/views/layout.blade.php
@@ -55,14 +55,25 @@
             <li class="navbar_menu_item"><a href="/contacts" class="navbar_menu_link">Contacts</a></li>
             <li class="navbar_menu_item"><a href="/more" class="navbar_menu_link">More</a></li>
             @auth
-                <li class="navbar_menu_item">
-                    <a href="/management" class="navbar_menu_link">Management</a>
-                </li>
-                <li class="navbar_menu_item">
-                    <a href="logout">
-                        <button class="loginButton" style="width:auto;">Logout</button>
-                    </a>
-                </li>
+                @if (Auth::user()->usertype=='volunteer')
+                    <li class="navbar_menu_item">
+                        <a href="/volunteer" class="navbar_menu_link">Volunteer</a>
+                    </li>
+                    <li class="navbar_menu_item">
+                        <a href="logout">
+                            <button class="loginButton" style="width:auto;">Logout</button>
+                        </a>
+                    </li>
+                @elseif (Auth::user()->usertype=='admin')
+                    <li class="navbar_menu_item">
+                        <a href="/management" class="navbar_menu_link">Management</a>
+                    </li>
+                    <li class="navbar_menu_item">
+                        <a href="logout">
+                            <button class="loginButton" style="width:auto;">Logout</button>
+                        </a>
+                    </li>
+                @endif
             @else
                 <li class="navbar_menu_item">
                     <button class="loginButton" onclick="document.getElementById('loginForm').style.display='block'" style="width:auto;">Login</button>
